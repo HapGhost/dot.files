d36b7ba8b5265b9387edbe25e194791b38fe0940
HapGhost <hapghost@hotmail.com>
2024-01-08 23:00:51 +0100

    fix mod...typos

--- nvidia/02-nvidia-install.sh
+++ nvidia/02-nvidia-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Nvidia driver
-sudo aptitude install nvidia-driver nvidia-smi nvidia-settings linux-headers firmware-misc-nonfree 
+sudo aptitude install nvidia-driver nvidia-smi nvidia-settings linux-headers-amd64 firmware-misc-nonfree 
 sudo cp ./nvidia-options.conf /etc/modeprobe.d
 ### DMA Buffers / IOMMU (https://us.download.nvidia.com/XFree86/Linux-x86_64/525.147.05/README/dma_issues.html)
 ### experimental add 'nvidia-drm.fbdev=1' below for >545.x.x driver
