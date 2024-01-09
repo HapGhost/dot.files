b0db2ee41bffb186589c6da3767510fd20671d52
HapGhost <hapghost@hotmail.com>
2024-01-09 10:38:23 +0100

    fixes in install scripts and apt configuration

--- debian/scripts/02-install.sh
+++ debian/scripts/02-install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo aptitude update; sudo aptitude install -t testing \
+sudo cp ../hyper-debian.list /etc/apt/sources.d/
+sudo cp ../hyper-debian /etc/apt/preferences.d/
+
+sudo aptitude update; sudo aptitude install -t testing -y \
   zram-tools micro \
   apt-transport-https \
   ca-certificates \
   apt-utils \
-  software-properties-common \
-  dirmngr \
-  dkms \
   git \
   zsh \
   zsh-autosuggestions \
@@ -26,13 +26,20 @@ sudo aptitude update; sudo aptitude install -t testing \
   apparmor apparmor-profiles apparmor-utils apparmor-profiles-extra\
   bind9-dnsutils \
   aptitude \
-  nftables \
+  nftables
   #ufw \
   #kmod \
   #cron \
   #netplan.io
 
-sudo aptitude update; sudo aptitude install -t unstable btrfs-progs cifs-utils -y
+sudo aptitude update; sudo aptitude install -t unstable -y /
+  btrfs-progs cifs-utils \
+  software-properties-common \
+  dirmngr \
+  dkms sbsigntool \
+  linux-image-amd64 linux-headers-amd64
+  
+sudo cp ../deb-multimedia.list /etc/apt/sources.d/
 
 sudo apt-get update -oAcquire::AllowInsecureRepositories=true
 sudo apt-get install deb-multimedia-keyring -y
@@ -40,4 +47,9 @@ sudo apt-get install deb-multimedia-keyring -y
 ####Enable Google BBR --- CHECK // NOT IN LXC
 curl -fsSL git.io/deploy-google-bbr.sh | bash
 
+### optimize zram swap
+sudo cp ../sysctl/99-vm-zram-parameters.conf /etc/sysctl.d/
+
 sudo aptitude safe-upgrade -y; sudo aptitude full-upgrade -y
+
+echo "Done...Please reboot!"
