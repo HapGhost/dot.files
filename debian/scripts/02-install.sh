6e6fa0ddab80ef8b5063489dfbeac679b4576d7c
HapGhost <hapghost@hotmail.com>
2024-01-09 11:04:46 +0100

    fixes again

--- debian/scripts/02-install.sh
+++ debian/scripts/02-install.sh
@@ -44,13 +44,14 @@ sudo aptitude update; sudo aptitude install -t unstable -y \
   software-properties-common \
   dirmngr \
   dkms sbsigntool \
-  linux-image-amd64 linux-headers-amd64
+  linux-image-amd64 linux-headers-amd64 firmware-linux \
+  pipewire \
+  wireplumber \
+  mesa-vulkan-drivers \
+  mesa-vdpau-drivers \
+  mesa-va-drivers \
+  libopengl0
   
-sudo cp ../apt/deb-multimedia.list /etc/apt/sources.list.d/
-
-sudo apt-get update -oAcquire::AllowInsecureRepositories=true
-sudo apt-get install deb-multimedia-keyring -y
-
 ####Enable Google BBR --- CHECK // NOT IN LXC
 curl -fsSL git.io/deploy-google-bbr.sh | bash
 
--- debian/scripts/03-install.sh
+++ debian/scripts/03-install.sh
@@ -8,9 +8,13 @@ sudo aptitude update; sudo aptitude install -t unstable -y \
   mesa-vdpau-drivers \
   mesa-va-drivers \
   libopengl0 \
-  firmware-linux
  
 ### to-do: Secure boot signing
 #wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg
 #echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-release.list
 #sudo aptitude update; sudo aptitude install linux-xanmod-x64v3 -y
+
+sudo cp ../apt/deb-multimedia.list /etc/apt/sources.list.d/
+
+sudo apt-get update -oAcquire::AllowInsecureRepositories=true
+sudo apt-get install deb-multimedia-keyring -y
