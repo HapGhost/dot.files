a7d16e9092126ebdba60b64127560848a4a7c22e
HapGhost <hapghost@hotmail.com>
2024-07-25 12:30:52 +0200

    updates

--- nvidia/00-run-nvidia.sh
+++ nvidia/00-run-nvidia.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-NVIDIA_VERSION=555.58.02
-wget https://download.nvidia.com/XFree86/Linux-x86_64/$NVIDIA_VERSION/NVIDIA-Linux-x86_64-$NVIDIA_VERSION.run
+NVIDIA_VERSION=560.28.03
+wget https://download.nvidia.com/XFree86/Linux-x86_64/$NVIDIA_VERSION/NVIDIA-Linux-x86_64-$NVIDIA_VERSION.run -O download/NVIDIA-Linux-x86_64-$NVIDIA_VERSION.run
 echo
 echo "Now 'sudo su' and start installation:"
-echo "  Proprietaty: sh ./NVIDIA-Linux-x86_64-$NVIDIA_VERSION.run"
-echo "  Open Source-Published: sh ./NVIDIA-Linux-x86_64-$NVIDIA_VERSION.run -m=kernel-open"
+echo "  Proprietaty: sh download/NVIDIA-Linux-x86_64-$NVIDIA_VERSION.run"
+echo "  Open Source-Published: sh download/NVIDIA-Linux-x86_64-$NVIDIA_VERSION.run -m=kernel-open"
